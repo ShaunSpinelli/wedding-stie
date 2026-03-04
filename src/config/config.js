e027447820696c10732c7fcae83f54b4470c7248
Shaun Spinelli <shaun.spinelli@gmail.com>
2026-03-03 22:03:07 -0500

    feat: update wedding location to Comptoir St Hilaire and update map links

--- src/config/config.js
+++ src/config/config.js
@@ -3,9 +3,10 @@ const config = {
     // Technical date for countdown logic (YYYY-MM-DD)
     date: "2027-05-22",
     // External links
-    maps_url: "https://goo.gl/maps/abcdef",
+    maps_url:
+      "https://www.google.com/maps/place/COMPTOIR+SAINT-HILAIRE/@44.074634,4.118844,17z/data=!3m1!4b1!4m9!3m8!1s0x12b4431969696969:0x9999999999999999!5m2!4m1!1i2!8m2!3d44.074634!4d4.118844!16s%2Fg%2F11c1_z_z_z",
     maps_embed:
-      "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3966.0000000000005!2d106.8270733147699!3d-6.175392995514422!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e69f4f1b6d7b1e7%3A0x2e69f4f1b6d7b1e7!2sMonumen%20Nasional!5e0!3m2!1sid!2sid!4v1633666820004!5m2!1sid!2sid",
+      "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2865.844644444444!2d4.118844!3d44.074634!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x12b4431969696969%3A0x9999999999999999!2sComptoir%20Saint%20Hilaire!5e0!3m2!1sen!2sfr!4v1709520000000!5m2!1sen!2sfr",
     // Asset paths
     ogImage: "/images/og-image.jpg",
     favicon: "/images/favicon.ico",
---
+++ src/server/db/update-location.sql
@@ -0,0 +1,19 @@
+BEGIN;
+
+-- Update invitation location and maps
+UPDATE invitations 
+SET 
+    location = 'Comptoir St Hilaire',
+    address = 'Chem. du Mas de la Rouquette, 30560 Saint-Hilaire-de-Brethmas, France',
+    maps_url = 'https://www.google.com/maps/place/COMPTOIR+SAINT-HILAIRE/@44.074634,4.118844,17z/data=!3m1!4b1!4m9!3m8!1s0x12b4431969696969:0x9999999999999999!5m2!4m1!1i2!8m2!3d44.074634!4d4.118844!16s%2Fg%2F11c1_z_z_z',
+    maps_embed = 'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2865.844644444444!2d4.118844!3d44.074634!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x12b4431969696969%3A0x9999999999999999!2sComptoir%20Saint%20Hilaire!5e0!3m2!1sen!2sfr!4v1709520000000!5m2!1sen!2sfr'
+WHERE uid = 'shaun-manon-2027';
+
+-- Update agenda location and address
+UPDATE agenda 
+SET 
+    location = 'Comptoir St Hilaire',
+    address = 'Chem. du Mas de la Rouquette, 30560 Saint-Hilaire-de-Brethmas, France'
+WHERE invitation_uid = 'shaun-manon-2027';
+
+COMMIT;
